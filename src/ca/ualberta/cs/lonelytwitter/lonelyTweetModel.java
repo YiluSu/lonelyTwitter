43a97b82d0a8826d0c065da132471f4d138b2c7d
YiluSu <yilu@ualberta.ca>
2014-01-14 18:12:26 -0700

    push

---
+++ src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
@@ -0,0 +1,38 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public abstract class lonelyTweetModel {
+    // public           anywhere
+    // protected        class + subclasses + package
+    // private          only current class + package
+    protected String text;
+    protected Date timestamp;
+    
+    public String getText() {
+        return text;
+    }
+    
+    public abstract void setText(String text);
+    
+    public Date getTimestamp() {
+        return timestamp;
+    }
+    
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+    public lonelyTweetModel(String text, Date timestamp) {
+        super();
+        this.text = text;
+        this.timestamp = timestamp;
+    }
+    
+    public lonelyTweetModel(String text) {
+        super();
+        this.text = text;
+        this.timestamp = new Date();
+    }
+    
+}
---
+++ src/ca/ualberta/cs/lonelytwitter/normalTweetModel.java
@@ -0,0 +1,17 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+
+public class normalTweetModel extends lonelyTweetModel
+{
+
+    public normalTweetModel(String text) {
+        super(text);
+    }
+
+    @Override
+    public void setText(String text) {
+    }
+
+}
