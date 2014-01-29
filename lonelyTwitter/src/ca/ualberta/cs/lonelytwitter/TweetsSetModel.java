810d4e94ea2eaeb15cdfd6ef7651f8dab17d742d
YiluSu <yilu@ualberta.ca>
2014-01-28 18:54:58 -0700

    test

---
+++ lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/TweetsSetModel.java
@@ -0,0 +1,25 @@
+package ca.ualberta.cs.lonelytwitter;
+
+
+public class TweetsSetModel
+{
+
+    int count = 0;
+    
+    public int countTweets()
+    {
+
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+
+
+    public void addTweet(NormalTweetModel normalTweetModel)
+    {
+
+        // TODO Auto-generated method stub
+        count++;
+    }
+    
+}
